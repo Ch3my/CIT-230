406cbbf1eb18349a89933dddac8875d871cc5cbb
Jose Miguel Concha <jmiguel@zenitx.com>
2018-02-19 10:25:29 -0300

    Fixed Math.pow

--- assignments/lesson-7/js/windchill.js
+++ assignments/lesson-7/js/windchill.js
@@ -2,5 +2,5 @@
 var t = 78; // average temp ( high + low / 2 ) in Fahrenheit
 var s = 5; // Wind Speed in miles per hour
 var f;
-f = 35.74 + 0.6215 * t - 35.75 * pow( s, 0.16 ) + 0.4275 * t * pow( s, 0.16 );
+f = 35.74 + 0.6215 * t - 35.75 * Math.pow( s, 0.16 ) + 0.4275 * t * Math.pow( s, 0.16 );
 document.getElementById("windchill").innerHTML = f;
