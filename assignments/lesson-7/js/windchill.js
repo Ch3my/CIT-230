a4e5e46d20e92349428a4147602a3ca162cb7ab0
Jose Miguel Concha <jmiguel@zenitx.com>
2018-02-19 10:31:14 -0300

    fix1

--- assignments/lesson-7/js/windchill.js
+++ assignments/lesson-7/js/windchill.js
@@ -3,4 +3,4 @@ var t = 78; // average temp ( high + low / 2 ) in Fahrenheit
 var s = 5; // Wind Speed in miles per hour
 var f;
 f = 35.74 + 0.6215 * t - 35.75 * Math.pow( s, 0.16 ) + 0.4275 * t * Math.pow( s, 0.16 );
-document.getElementById("windchill").innerHTML = f;
+document.getElementById("windChill").innerHTML = f;
