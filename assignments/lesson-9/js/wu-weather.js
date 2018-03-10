f91583d8f09ef0c8e23e9c672d0efb3eaa161d99
Jose Miguel Concha <jmiguel@zenitx.com>
2018-03-10 15:45:47 -0300

    Fix4

--- assignments/lesson-9/js/wu-weather.js
+++ assignments/lesson-9/js/wu-weather.js
@@ -19,13 +19,13 @@ function showData ( townData ) {
     var textDescrip = document.createElement( 'p' );
 
     sCurrentWeather.textContent = townData[ 'current_observation' ]['weather'];
-    currentTemp.textContent = townData[ 'current_observation' ]['temp_f'];
-    currentWindSpeed.textContent = townData[ 'current_observation' ]['wind_mph'];
+    currentTemp.textContent = 'Current temp ' +townData[ 'current_observation' ]['temp_f'] + ' F';
+    currentWindSpeed.textContent = 'Wind Speed ' + townData[ 'current_observation' ]['wind_mph'] + ' mph';
     icon.setAttribute('src', townData[ 'current_observation' ]['icon_url'] );
     icon.setAttribute('alt', townData[ 'current_observation' ]['icon'] );
     textDescrip.textContent = townData[ 'forecast' ]['txt_forecast']['forecastday'][0]['fcttext'] + ' Low ' +
-            townData['forecast'][ 'simpleforecast' ]['forecastday'][0]['low']['fahrenheit'] + ' High ' +
-            townData['forecast'][ 'simpleforecast' ]['forecastday'][0]['high']['fahrenheit'];
+            townData['forecast'][ 'simpleforecast' ]['forecastday'][0]['low']['fahrenheit'] + ' F High ' +
+            townData['forecast'][ 'simpleforecast' ]['forecastday'][0]['high']['fahrenheit'] + 'F';
 
     article.appendChild(sCurrentWeather);
     article.appendChild(currentTemp);
